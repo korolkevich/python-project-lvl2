37b7f7b1f5d77e0c4fb37ec2d803b64c0723ca25
Mikhail Korolkevich <m.korolkevich@xsolla.com>
2021-02-21 21:08:46 +0500

    fix: second step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,18 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate diff.')
-parser.add_argument(
-    'first_file',
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff.')
+    parser.add_argument('first_file')
 
-)
+    parser.add_argument('second_file')
 
-parser.add_argument(
-    'second_file',
+    parser.add_argument(
+        '-f',
+        '--format',
+    )
 
-)
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
 
-parser.add_argument(
-    '-f',
-    '--format',
-)
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+if __name__ == '__main__':
+    main()
